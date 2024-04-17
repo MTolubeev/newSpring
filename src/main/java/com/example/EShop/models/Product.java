30c9512f681bc8f66c7401a4bde4210592f8f20f
mtolubeev <mtolubeev@nicetu.spb.ru>
2024-04-17 15:17:39 +0300

    first commit

---
+++ src/main/java/com/example/EShop/models/Product.java
@@ -0,0 +1,35 @@
+package com.example.EShop.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "count")
+    private int count;
+
+    @Column(name = "user")
+    private String user;
+}
