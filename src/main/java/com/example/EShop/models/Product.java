6134ba9a79b7eb613dfc5ef994302b7f77c39c06
mtolubeev <mtolubeev@nicetu.spb.ru>
2024-04-18 15:15:43 +0300

    second commit

--- src/main/java/com/example/EShop/models/Product.java
+++ src/main/java/com/example/EShop/models/Product.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.imageio.stream.ImageInputStreamImpl;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "products")
 @Data
@@ -14,7 +19,6 @@ import lombok.NoArgsConstructor;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-
     @Column(name = "id")
     private Long id;
 
@@ -30,6 +34,21 @@ public class Product {
     @Column(name = "count")
     private int count;
 
-    @Column(name = "user")
-    private String user;
+    @Column(name = "shopper")
+    private String shopper;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init(){
+        dateOfCreated = LocalDateTime.now();
+    }
+    public void addImageToProduct(Image image){
+        image.setProduct(this);
+        images.add(image);
+    }
 }
--- src/main/java/com/example/EShop/services/ProductService.java
+++ src/main/java/com/example/EShop/services/ProductService.java
@@ -1,11 +1,14 @@
 package com.example.EShop.services;
 
+import com.example.EShop.models.Image;
 import com.example.EShop.models.Product;
 import com.example.EShop.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,13 +20,41 @@ public class ProductService {
     private final List<Product> products = new ArrayList<>();
 
     public List<Product> listProducts(String title) {
-        if (title != null) productRepository.findByTitle(title);
+        if (title != null) return productRepository.findByTitle(title);
         return productRepository.findAll();
     }
 
-    public void saveProduct(Product product) {
-        log.info("Saving new {}", product);
+    public void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+        Image image1;
+        Image image2;
+        Image image3;
+        if (file1.getSize() != 0) {
+            image1 = toImageEntity(file1);
+            image1.setPreviewImage(true);
+            product.addImageToProduct(image1);
+        }
+        if (file2.getSize() != 0) {
+            image2 = toImageEntity(file2);
+            product.addImageToProduct(image2);
+        }
+        if (file3.getSize() != 0) {
+            image3 = toImageEntity(file3);
+            product.addImageToProduct(image3);
+        }
+        log.info("Saving new Product. Title: {}; Shopper: {}", product.getTitle(), product.getShopper());
+        Product productFromDB = productRepository.save(product);
+        productFromDB.setPreviewImageId(productFromDB.getImages().get(0).getId());
         productRepository.save(product);
+    }
+
+    private Image toImageEntity(MultipartFile file) throws IOException {
+        Image image = new Image();
+        image.setName(file.getName());
+        image.setOriginalFileName(file.getOriginalFilename());
+        image.setContentType(file.getContentType());
+        image.setSize(file.getSize());
+        image.setBytes(file.getBytes());
+        return image;
     }
 
     public void deleteProduct(Long id) {
