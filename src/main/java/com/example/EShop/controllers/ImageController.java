b45d3dacd5bb13e38e8944ecd7794ae805d1612a
mtolubeev <mtolubeev@nicetu.spb.ru>
2024-07-30 15:51:15 +0300

    images

--- src/main/java/com/example/EShop/controllers/ImageController.java
+++ src/main/java/com/example/EShop/controllers/ImageController.java
@@ -1,16 +1,15 @@
 package com.example.EShop.controllers;
 
+import com.example.EShop.dtos.ImageDto;
 import com.example.EShop.models.Image;
 import com.example.EShop.repositories.ImageRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.ByteArrayInputStream;
+import java.util.Base64;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,15 +17,22 @@ public class ImageController {
     private final ImageRepository imageRepository;
 
     @GetMapping("/images/{id}")
-    private ResponseEntity<?> getImageById(@PathVariable Long id) {
+    private ResponseEntity<ImageDto> getImageById(@PathVariable Long id) {
         Image image = imageRepository.findById(id).orElse(null);
         if (image == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok()
-                .header("fileName", image.getOriginalFileName())
-                .contentType(MediaType.valueOf(image.getContentType()))
-                .contentLength(image.getSize())
-                .body(new InputStreamResource(new ByteArrayInputStream(image.getBytes())));
+
+        String base64Image = Base64.getEncoder().encodeToString(image.getBytes());
+
+        ImageDto imageDto = new ImageDto(
+                image.getId(),
+                image.getOriginalFileName(),
+                image.getContentType(),
+                image.getSize(),
+                base64Image
+        );
+
+        return ResponseEntity.ok(imageDto);
     }
 }
