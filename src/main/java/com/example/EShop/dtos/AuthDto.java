09ad479697e0da0e842b7a3b005ea9df8192d783
mtolubeev <mtolubeev@nicetu.spb.ru>
2024-08-13 18:22:34 +0300

    correct user2

--- src/main/java/com/example/EShop/dtos/AuthDto.java
+++ src/main/java/com/example/EShop/dtos/AuthDto.java
@@ -2,10 +2,6 @@ package com.example.EShop.dtos;
 
 import com.example.EShop.models.Comment;
 import com.example.EShop.models.User;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -19,7 +15,6 @@ public class AuthDto {
     private String username;
     private String surname;
     private String role;
-    private List<Comment> comments;
     private String token;
 
     public AuthDto(User user, String token) {
@@ -28,7 +23,6 @@ public class AuthDto {
         this.username = user.getUsername();
         this.surname = user.getSurname();
         this.role = user.getRoles().toString().replace("[", "").replace("]", "");
-        this.comments = user.getComments();
 
         this.token = token;
     }
