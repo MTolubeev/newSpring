c479a779a4fa79c35c1a92607738aaa698b3c9ca
Меляшевич Елена Игоревна <emelyashevich@nic.etu>
2024-08-26 19:55:48 +0300

    design

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,10 +1,13 @@
 <template>
+  <div class="modal-overlay">
+    <div class="modal-content">
     <form>
         <input type="text">
         <input type="text">
         <button type="button" @click="emitClose">закрыть</button>
     </form>
-
+  </div>
+  </div>
 </template>
 
 <script setup>
@@ -19,6 +22,25 @@ const emitClose = () => {
 </script>
 
 <style scoped>
+.modal-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 99;
+}
 
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  width: 300px;
+  z-index: 100;
+}
 
 </style>
